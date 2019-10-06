919b2825a264b9eed0062092eccb67529dc40d58
xiangxu <iasonliu@gmail.com>
2019-10-06 18:38:46 +0800

    adding styling with css

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,8 +1,8 @@
 import React from 'react';
-
+import './Person.css';
 const person = ({ name, age, click, changed, children }) => {
   return (
-    <div>
+    <div className="Person">
       <p onClick={click}>
         I'm a {name}! I am {age} years old!
       </p>
