5dc3e411a48f92eff8cab555248ebd65cecd8f70
xiangxu <iasonliu@gmail.com>
2019-10-06 17:13:32 +0800

    add events handler

--- src/App.js
+++ src/App.js
@@ -4,27 +4,17 @@ import Person from './Person/Person';
 
 class App extends Component {
   state = {
-    persons: [
-      {
-        name: 'Max',
-        age: 28,
-      },
-      {
-        name: 'Manu',
-        age: 29,
-      },
-      {
-        name: 'Stephanie',
-        age: 26,
-      },
-    ],
+    persons: [{ name: 'Max', age: 28 }, { name: 'Manu', age: 29 }, { name: 'Stephanie', age: 26 }],
+  };
+  switchNameHandler = () => {
+    console.log('Was clicked!');
   };
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is really working!</p>
-        <button>Switch Name</button>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
         <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
         <Person name={this.state.persons[1].name} age={this.state.persons[1].name}>
           My Hobbies: Racing
