2567948522bd2ac3178a1e3cfb0ce65dae8b5d79
xiangxu <iasonliu@gmail.com>
2019-10-06 19:20:56 +0800

    Handling dynamic content

--- src/App.js
+++ src/App.js
@@ -40,6 +40,23 @@ class App extends Component {
       padding: '8px',
       cursor: 'pointer',
     };
+    let persons = null;
+    if (this.state.showPersons) {
+      persons = (
+        <div>
+          <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+          <Person
+            name={this.state.persons[1].name}
+            age={this.state.persons[1].age}
+            click={this.switchNameHandler.bind(this, 'Max')}
+            changed={this.nameChangeHandler}
+          >
+            My Hobbies: Racing
+          </Person>
+          <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
+        </div>
+      );
+    }
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
@@ -47,20 +64,7 @@ class App extends Component {
         <button style={style} onClick={this.togglePersonsHandler}>
           Toggle Persons
         </button>
-        {this.state.showPersons ? (
-          <div>
-            <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
-            <Person
-              name={this.state.persons[1].name}
-              age={this.state.persons[1].age}
-              click={this.switchNameHandler.bind(this, 'Max')}
-              changed={this.nameChangeHandler}
-            >
-              My Hobbies: Racing
-            </Person>
-            <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
-          </div>
-        ) : null}
+        {persons}
       </div>
     );
   }
