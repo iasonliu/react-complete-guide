2d3358bed7eb67aa8f06e12bf341d1bf2b111c44
xiangxu <iasonliu@gmail.com>
2019-10-06 17:18:43 +0800

    set State

--- src/App.js
+++ src/App.js
@@ -5,9 +5,21 @@ import Person from './Person/Person';
 class App extends Component {
   state = {
     persons: [{ name: 'Max', age: 28 }, { name: 'Manu', age: 29 }, { name: 'Stephanie', age: 26 }],
+    otherState: 'some other value',
   };
   switchNameHandler = () => {
-    console.log('Was clicked!');
+    // console.log('Was clicked!');
+
+    // DON'T DO THIS:
+    // this.state.persons[0].name = 'Maximilian';
+
+    this.setState({
+      persons: [
+        { name: 'Maximilian', age: 28 },
+        { name: 'Manu', age: 29 },
+        { name: 'Stephanie', age: 27 },
+      ],
+    });
   };
   render() {
     return (
@@ -16,10 +28,10 @@ class App extends Component {
         <p>This is really working!</p>
         <button onClick={this.switchNameHandler}>Switch Name</button>
         <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].name}>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>
           My Hobbies: Racing
         </Person>
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].name} />
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
       </div>
     );
     // return React.createElement(
