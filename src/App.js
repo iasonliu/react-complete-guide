5067c416d01903e28ee6612e3db90db863653bdc
xiangxu <iasonliu@gmail.com>
2019-10-06 18:32:54 +0800

    changed events

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ class App extends Component {
   state = {
     persons: [{ name: 'Max', age: 28 }, { name: 'Manu', age: 29 }, { name: 'Stephanie', age: 26 }],
   };
+
   switchNameHandler = (newName) => {
     this.setState({
       persons: [
@@ -15,28 +16,34 @@ class App extends Component {
       ],
     });
   };
+
+  nameChangeHandler = (event) => {
+    this.setState({
+      persons: [
+        { name: 'Max', age: 28 },
+        { name: event.target.value, age: 29 },
+        { name: 'Stephanie', age: 26 },
+      ],
+    });
+  };
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is really working!</p>
-        <button onClick={() => this.switchNameHandler('Maxmlilan!')}>Switch Name</button>
+        <button onClick={() => this.switchNameHandler('Maxmlilan')}>Switch Name</button>
         <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
         <Person
           name={this.state.persons[1].name}
           age={this.state.persons[1].age}
-          click={this.switchNameHandler.bind(this, 'Max!')}
+          click={this.switchNameHandler.bind(this, 'Max')}
+          changed={this.nameChangeHandler}
         >
           My Hobbies: Racing
         </Person>
         <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
       </div>
     );
-    // return React.createElement(
-    //   'div',
-    //   null,
-    //   React.createElement('h1', { className: 'App' }, "Hi, I'm a other React App!!!"),
-    // );
   }
 }
 
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,12 +1,13 @@
 import React from 'react';
 
-const person = ({ name, age, children }) => {
+const person = ({ name, age, click, changed, children }) => {
   return (
     <div>
-      <p>
+      <p onClick={click}>
         I'm a {name}! I am {age} years old!
       </p>
       <p>{children}</p>
+      <input type="test" onChange={changed} value={name} />
     </div>
   );
 };
