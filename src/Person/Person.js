76b0c7c08daca570f00df070a1086c6efcd37f41
xiangxu <iasonliu@gmail.com>
2019-10-06 21:49:52 +0800

    enable css modules

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
-import './Person.css';
+import classes from './Person.css';
 const person = ({ name, age, click, changed, children }) => {
   return (
-    <div className="Person">
+    <div className={classes.Person}>
       <p onClick={click}>
         I'm a {name}! I am {age} years old!
       </p>
