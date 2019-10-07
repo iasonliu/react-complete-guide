363b55089aca5b0f2c92f4ab0dcc0b4ceabf68b6
xiangxu <iasonliu@gmail.com>
2019-10-07 16:53:46 +0800

    component updating lifecycle for state changes

--- src/containers/App.js
+++ src/containers/App.js
@@ -26,6 +26,13 @@ class App extends Component {
   componentDidMount() {
     console.log('[App.js] componentDidMount');
   }
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log('[App.js] shouldComponentUpdate');
+    return true;
+  }
+  componentDidUpdate() {
+    console.log('[App.js] componentDidUpdate');
+  }
   deletePersonHandler = (personIndex) => {
     // const persons = this.state.persons.slice();
     const persons = [...this.state.persons];
