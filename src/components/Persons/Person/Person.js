f2dc7b65a657c215d8bd35434bdbb26de6219831
xiangxu <iasonliu@gmail.com>
2019-10-12 11:19:29 +0800

    return list JSX elements

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -3,18 +3,15 @@ import React, { Component } from 'react';
 import classes from './Person.css';
 
 class Person extends Component {
- 
   render() {
     console.log('[Person.js] rendering...');
-    return (
-      <div className={classes.Person}>
-        <p onClick={this.props.click}>
-          I'm a {this.props.name}! I am {this.props.age} years old!
-        </p>
-        <p>{this.props.children}</p>
-        <input type="text" onChange={this.props.changed} value={this.props.name} />
-      </div>
-    );
+    return [
+      <p key="i1" onClick={this.props.click}>
+        I'm a {this.props.name}! I am {this.props.age} years old!
+      </p>,
+      <p key="i2">{this.props.children}</p>,
+      <input key="i3" type="text" onChange={this.props.changed} value={this.props.name} />,
+    ];
   }
 }
 
